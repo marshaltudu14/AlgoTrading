b35b6f34ee95a1978102935c31e337fde0654edc
Marshal <iammarshal22@gmail.com>
2025-07-30 14:12:26 +0530

    Remove venv directory from repository

--- activate_venv.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Activating AlgoTrading virtual environment...
-call venv\Scripts\activate.bat
-echo Virtual environment activated!
-echo You can now run: python run_training.py
-cmd /k
--- src/config/instrument.py
+++
@@ -1,9 +0,0 @@
-class Instrument:
-    def __init__(self, symbol: str, type: str, lot_size: int, tick_size: float):
-        self.symbol = symbol
-        self.type = type
-        self.lot_size = lot_size
-        self.tick_size = tick_size
-
-    def __repr__(self):
-        return f"Instrument(symbol='{self.symbol}', type='{self.type}', lot_size={self.lot_size}, tick_size={self.tick_size})"
